867ca28f14c8f929c86080cc4d1cf87028f2320a
Justin Green <justingreenmedia@gmail.com>
2021-10-28 17:02:07 -0500

    Updated particles

--- js/particles-init.js
+++ js/particles-init.js
@@ -1,32 +1,6 @@
 Particles.init({
 	selector: '.particle-bg',
 	color: ['#fff', '#29AB87'],
-	maxParticles: 200,
-	speed: 1,
-	responsive: [
-		{
-			breakpoint: 768,
-			options: {
-				maxParticles: 0
-			}
-		},
-		{
-			breakpoint: 425,
-			options: {
-				maxParticles: 100
-			}
-		},
-		{
-			breakpoint: 375,
-			options: {
-				maxParticles: 100
-			}
-		},
-		{
-			breakpoint: 320,
-			options: {
-				maxParticles: 100
-			}
-		}
-	]
+	maxParticles: 150,
+	speed: 1
 });	
